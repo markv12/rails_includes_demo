b2ce66b78bd3cfaf82be7fd8b77e466bbd4349be
Scott Steffes <swsteffes@gmail.com>
2021-11-04 00:38:06 -0500

    add full tables

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,5 +2,13 @@ class DemoController < ApplicationController
   def index
     
   end
+
+  def all_no_includes
+    @cities = City.all
+  end
+
+  def all_full_includes
+    @cities = City.includes({ authors: {articles: :comments}}).all
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'demo#index'
+  match '/all_no_includes', to: 'demo#all_no_includes', as: 'all_no_includes', via: :get
+  match '/all_full_includes', to: 'demo#all_full_includes', as: 'all_full_includes', via: :get
   
 end
